85c8be7d550b56acb3d80bc182671513bcd66dd2
krutimistry <km487@njit.edu>
2019-03-17 14:48:53 -0400

    Step 2 - swapPhotos function edited

--- js/gallery.js
+++ js/gallery.js
@@ -41,41 +41,26 @@ function GalleryImage(location, description, date, image) {
 
 //$GET Request
 
+// Counter for the mImages array
+var mCurrentIndex = 0;
+
 //swapPhotos functions
 function swapPhoto() {
-	if(prevClicked){
-		if(mCurrentIndex > 0) {
-			mCurrentIndex--;
-		} else {
-			mCurrentIndex = mImages.length-1;
-		};
-	}else {
-		if(mCurrentIndex < mImages.length-1) {
+	if (!prevClicked) {
+		if (mCurrentIndex < mImages.length - 1) {
 			mCurrentIndex++;
 		} else {
 			mCurrentIndex = 0;
-		};
+		}
+		;
+	} else {
+		if (mCurrentIndex > 0) {
+			mCurrentIndex--;
+		} else {
+			mCurrentIndex = mImages.length - 1;
+		}
+		;
 	};
-
-	setPhoto();
-};
-
-// Counter for the mImages array
-var mCurrentIndex = 0;
-function swapPhoto() {
-    if(prevClicked){
-        if(mCurrentIndex > 0) {
-            mCurrentIndex--;
-        } else {
-            mCurrentIndex = mImages.length-1;
-        };
-    }else {
-        if(mCurrentIndex < mImages.length-1) {
-            mCurrentIndex++;
-        } else {
-            mCurrentIndex = 0;
-        };
-    };
 
     setPhoto();
 };
